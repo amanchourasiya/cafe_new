7e52bc2931e2c402b85bb973c2476fecd67e3d4e
amanchourasiya <amanchourasia6@gmail.com>
2018-03-15 10:45:14 +0530

    AMan

--- app/src/main/java/ribboncafe/ribbon/com/ribboncafe/FoodDetail.java
+++ app/src/main/java/ribboncafe/ribbon/com/ribboncafe/FoodDetail.java
@@ -4,8 +4,10 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.google.firebase.database.DataSnapshot;
@@ -15,7 +17,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import Database.Database;
 import Model.Foods;
+import Model.Order;
 
 public class FoodDetail extends AppCompatActivity {
 
@@ -31,7 +35,7 @@ public class FoodDetail extends AppCompatActivity {
     DatabaseReference foods;
 
 
-
+    Foods currentFood;
 
 
     @Override
@@ -44,6 +48,21 @@ public class FoodDetail extends AppCompatActivity {
 
         numberButton=(ElegantNumberButton)findViewById(R.id.number_button);
         btncart=(FloatingActionButton)findViewById(R.id.btnCart);
+
+        btncart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Database(getBaseContext()).addToCart(new Order(
+                        foodId,
+                        currentFood.getName(),
+                        numberButton.getNumber(),
+                        currentFood.getPrice()
+
+                ));
+                Toast.makeText(FoodDetail.this,"Added to Cart",Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         food_description=(TextView)findViewById(R.id.food_description);
         food_name=(TextView)findViewById(R.id.food_name);
@@ -70,11 +89,11 @@ public class FoodDetail extends AppCompatActivity {
         foods.child(foodId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Foods food = dataSnapshot.getValue(Foods.class);
-                Picasso.with(getBaseContext()).load(food.getImage()).into(food_image);
-                collapsingToolbarLayout.setTitle(food.getName());
-                food_price.setText(food.getPrice());
-                food_name.setText(food.getName());
+                currentFood = dataSnapshot.getValue(Foods.class);
+                Picasso.with(getBaseContext()).load(currentFood.getImage()).into(food_image);
+                collapsingToolbarLayout.setTitle(currentFood.getName());
+                food_price.setText(currentFood.getPrice());
+                food_name.setText(currentFood.getName());
                 //food description
 
             }
--- app/src/main/java/ribboncafe/ribbon/com/ribboncafe/nav_drawer.java
+++ app/src/main/java/ribboncafe/ribbon/com/ribboncafe/nav_drawer.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -63,8 +62,8 @@ public class nav_drawer extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               Intent cartIntent=new Intent(nav_drawer.this,Cart.class);
+               startActivity(cartIntent);
             }
         });
 
