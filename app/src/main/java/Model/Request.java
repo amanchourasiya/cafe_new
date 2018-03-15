9cfe0fb287a7a03bf3d595a2eb945a9a4486a72e
amanchourasiya <amanchourasia6@gmail.com>
2018-03-15 10:47:23 +0530

    AMan

---
+++ app/src/main/java/Model/Request.java
@@ -0,0 +1,57 @@
+package Model;
+
+import java.util.List;
+
+/**
+ * Created by mayankkataruka on 15/03/18.
+ */
+
+public class Request {
+
+    private String email,note,total;
+    private List<Order> foods;
+
+    public Request()
+    {
+
+    }
+
+    public Request(String email, String note, String total, List<Order> foods) {
+        this.email = email;
+        this.note = note;
+        this.total = total;
+        this.foods = foods;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public List<Order> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(List<Order> foods) {
+        this.foods = foods;
+    }
+}
